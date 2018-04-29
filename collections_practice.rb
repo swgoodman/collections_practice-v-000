f09017999cacc55f9783f2ab19272a15c5d0f8fb
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-29 18:46:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,8 +16,9 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  second = array[1]
-  third = array[2]
-  second = third
-  
+  if array [2]
+end
+
+def reverse_array(array)
+  array.reverse
 end
