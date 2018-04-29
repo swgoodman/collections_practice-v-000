1999d795d5478d169687f139c6d3696c0c7844f7
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-29 18:40:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,6 +15,6 @@ def sort_array_char_count(array)
   array.sort_by { |item| item.length}
 end
 
-def swap!(array)
-  array[1], array[2] = array[2], array[1]
+def swap_elements(array)
+  array.swap!(2,3)
 end
